64a450fd2b292ddf26f37aeed510b24b9c516970
Sunghyun Kwon <hobanbird@gmail.com>
2021-11-13 23:12:22 +0900

    Add: clone coding todo app for react

---
+++ react/todo-app/src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useRef, useCallback } from 'react';
+import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos
+    // [
+    // {
+    //   id: 1,
+    //   text: '리엑트 기초 알아보기',
+    //   checked: true,
+    // },
+    // {
+    //   id: 2,
+    //   text: '컴포넌트 스타일링해 보기',
+    //   checked: true,
+    // },
+    // {
+    //   id: 3,
+    //   text: '일정 관리 앱 만들어 보기',
+    //   checked: true,
+    // },
+    // ]
+  );
+
+  const nextId = useRef(2501);
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos => todos.concat(todo));
+      nextId.current += 1;
+    }, 
+    []
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos => todos.filter(todo => todo.id !== id));
+    },
+    [],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos => todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo)
+      );
+    },
+    [],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  )
+}
+
+export default App;
